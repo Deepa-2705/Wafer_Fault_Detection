dfbd0d1cb011f321718f2f518313a530609a5b61
Deepa-2705 <deepagupta35795@gmail.com>
2024-03-31 23:44:53 +0530

    Model Training and Evaluation done

--- app.py
+++ app.py
@@ -47,6 +47,6 @@ def upload():
         raise CustomException(e,sys)
     
 
-# execution will start from here
+# execution will start from here (added this comment to add into github)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug= True)
